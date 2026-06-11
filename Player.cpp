c36c0029e928f61441692a7c1d8b60593bf2bc71
Monster20-cpu <aleksanderkujawa74@gmail.com>
2026-06-12 01:30:09 +0200

    Konfiguracja elemetów klasy Player oraz mechaniki wyniku

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,26 @@
 #include "Player.h"
+#include "Config.h"
 
-void Player::update(float dt, float scrollSpeed) {}
-void Player::draw(sf::RenderWindow& window) {}
-sf::FloatRect Player::getBounds() const { return sf::FloatRect(); }
+Player::Player() : velocity(0.f, 0.f), grounded(false), isGliding(false),
+    isDucking(false), isSmashing(false), hasShield(false), score(0.f),
+    glideTimer(0.f), lastSTime(-10.f) {
+    shape.setSize(sf::Vector2f(60.f, 80.f));
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(100.f, GROUND_Y - 80.f);
+}
+
+void Player::adjustShapeToState() {
+    shape.setSize(sf::Vector2f(60.f, 80.f));
+    shape.setFillColor(sf::Color::Green);
+}
+
+void Player::update(float dt, float scrollSpeed) {
+    // Logika ruchu w Milestone 3
+}
+
+void Player::draw(sf::RenderWindow& window) { window.draw(shape); }
+sf::FloatRect Player::getBounds() const { return shape.getGlobalBounds(); }
+
+int Player::getScore() const { return static_cast<int>(score); }
+void Player::addScore(float s) { score += s; }
+void Player::setScore(float s) { score = s; }
--- Player.h
+++ Player.h
@@ -1,9 +1,34 @@
 #pragma once
 #include "GameObject.h"
+#include <SFML/Window/Event.hpp>
 
 class Player : public GameObject {
+private:
+    sf::RectangleShape shape;
+    sf::Vector2f velocity;
+
+    bool grounded;
+    bool isGliding;
+    bool isDucking;
+    bool isSmashing;
+    bool hasShield;
+
+    float score;
+    float glideTimer;
+
+    sf::Clock tapClock;
+    float lastSTime;
+
+    void adjustShapeToState();
+
 public:
+    Player();
+
     void update(float dt, float scrollSpeed) override;
     void draw(sf::RenderWindow& window) override;
     sf::FloatRect getBounds() const override;
+
+    int getScore() const;
+    void addScore(float s);
+    void setScore(float s);
 };
